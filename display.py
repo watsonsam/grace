28a86befcb3871f731e1f93d8aba9a8382abfcd3
Sam Watson <watsonsam@yahoo.co.uk>
2020-10-26 22:15:17 +0000

    use Surface instead of display use colours

--- display.py
+++ display.py
@@ -1,33 +1,28 @@
 import pygame
+
 pygame.init()
 
 
 def draw_maze(maze, cell_size):
-    running = True
-    screen = pygame.display.set_mode([(maze.width * cell_size) + 1, (maze.height * cell_size) + 1])
+    screen = pygame.Surface([(maze.width * cell_size) + 1, (maze.height * cell_size) + 1])
     screen.fill((255, 255, 255))
 
     for k, v in maze.grid.items():
         x, y = k
+        line_colour = (255, 100, 255)
         if v.north:
-            pygame.draw.line(screen, (0, 0, 0), (x * cell_size, y * cell_size),
+            pygame.draw.line(screen, line_colour, (x * cell_size, y * cell_size),
                              ((x * cell_size) + cell_size, y * cell_size), 3)
         if v.south:
-            pygame.draw.line(screen, (0, 0, 0), (x * cell_size, (y * cell_size) + cell_size),
+            pygame.draw.line(screen, line_colour, (x * cell_size, (y * cell_size) + cell_size),
                              ((x * cell_size) + cell_size, (y * cell_size) + cell_size), 3)
         if v.east:
-            pygame.draw.line(screen, (0, 0, 0), ((x * cell_size) + cell_size, y * cell_size),
+            pygame.draw.line(screen, line_colour, ((x * cell_size) + cell_size, y * cell_size),
                              ((x * cell_size) + cell_size, (y * cell_size) + cell_size), 3)
         if v.west:
-            pygame.draw.line(screen, (0, 0, 0), (x * cell_size, y * cell_size),
+            pygame.draw.line(screen, line_colour, (x * cell_size, y * cell_size),
                              (x * cell_size, (y * cell_size) + cell_size), 3)
 
-    pygame.display.flip()
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
+    pygame.image.save(screen, "test.png")
 
     pygame.quit()
--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import display
 
 
 def main():
-    for i in range(3):
-        the_maze = maze.Maze(30, 50)
-        display.draw_maze(the_maze, 30)
-        print("***************")
+    the_maze = maze.Maze(32, 50)
+    display.draw_maze(the_maze, 20)
 
 if __name__ == '__main__':
     main()
